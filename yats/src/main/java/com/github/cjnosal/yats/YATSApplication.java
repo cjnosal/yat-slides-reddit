5bd009b951dfd6db94b88aad9b80122f62666d5a
Conor Nosal <c.nosal@gmail.com>
2016-04-01 22:37:34 -0400

    Add StrictMode and LeakCanary checks

--- yats/src/main/java/com/github/cjnosal/yats/YATSApplication.java
+++ yats/src/main/java/com/github/cjnosal/yats/YATSApplication.java
@@ -1,10 +1,12 @@
 package com.github.cjnosal.yats;
 
 import android.app.Application;
+import android.os.StrictMode;
 
 import com.github.cjnosal.yats.modules.ApplicationComponent;
 import com.github.cjnosal.yats.modules.ApplicationModule;
 import com.github.cjnosal.yats.modules.DaggerApplicationComponent;
+import com.squareup.leakcanary.LeakCanary;
 
 import timber.log.Timber;
 
@@ -15,7 +17,15 @@ public class YATSApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Timber.plant(new Timber.DebugTree());
+
+        // Debugging
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().detectAll().penaltyLog().build());
+            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder().detectAll().penaltyLog().build());
+            LeakCanary.install(this);
+        }
+        // Injection
         ApplicationModule applicationModule = new ApplicationModule(getApplicationContext());
         applicationComponent = DaggerApplicationComponent.builder().applicationModule(applicationModule).build();
     }

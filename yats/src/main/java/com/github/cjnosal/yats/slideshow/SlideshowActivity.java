91c642054010d2b1f9e56c8bc46534864f769ebd
Conor Nosal <c.nosal@gmail.com>
2016-05-01 14:28:16 -0400

    Add immersive mode to SlideshowActivity

--- yats/src/main/java/com/github/cjnosal/yats/slideshow/SlideshowActivity.java
+++ yats/src/main/java/com/github/cjnosal/yats/slideshow/SlideshowActivity.java
@@ -1,12 +1,14 @@
 package com.github.cjnosal.yats.slideshow;
 
 import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.ColorInt;
 import android.support.v4.graphics.ColorUtils;
 import android.support.v4.view.ViewPager;
 import android.support.v7.graphics.Palette;
 import android.util.TypedValue;
+import android.view.View;
 
 import com.github.cjnosal.yats.R;
 import com.github.cjnosal.yats.YATSApplication;
@@ -96,6 +98,21 @@ public class SlideshowActivity extends RxAppCompatActivity implements SlideshowC
             slidePager.setCurrentItem(savedInstanceState.getInt(CURRENT_SLIDE), false);
         } else {
             presenter.findImages();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         }
     }
 
